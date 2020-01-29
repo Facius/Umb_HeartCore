11aa4f3a5f85eb5bd94361959e2d32cd36ff3e20
Victor Facius <vfa@illumi.dk>
2020-01-29 19:26:55 +0100

    prettier en performance test

--- victor.ts
+++ victor.ts
@@ -1,12 +1,24 @@
-import { Client } from "@umbraco/headless-client";
-
+import { Client, ContentResponseElement, ContentManagerMediaType, Media } from '@umbraco/headless-client';
+import { performance } from 'perf_hooks';
+interface IPageContent extends ContentResponseElement {
+    title: string;
+    bodyText: string;
+    image: Media;
+}
 export class Victor {
+  constructor(client: Client) {
+      const t1 = performance.now();
 
-    constructor(client: Client) {
-        client.delivery.content.byUrl('/').then((data)=> console.log(data));
+    client.delivery.content.byUrl<IPageContent>('/').then(data => {
+      client.delivery.content.children(data._id).then(c => c.items.forEach(c2 => {
+        client.delivery.content.children(data._id).then(c => {
+            console.log(Math.round(performance.now() - t1));
+        });
+      }));
+    }); 
 
-        client.delivery.media.root().then()
-        
-
-    }
+    // client.delivery.media.root().then(m => {
+    //     console.log(m);
+    // })
+  }
 }
